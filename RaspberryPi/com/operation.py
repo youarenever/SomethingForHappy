e16c82ffbe24c4f484293bd9ad45ca6c504f39e4
youarenever <395390646@qq.com>
2017-09-07 22:08:35 +0800

    flask hello ok

--- RaspberryPi/com/operation.py
+++ RaspberryPi/com/operation.py
@@ -1,15 +1,16 @@
 # coding=utf-8
 from flask import Flask, request, render_template
 
-# from RaspberryPi.com.home.helloworld import hello_world_main
+from RaspberryPi.com.home.helloworld import hello_world_main
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    return render_template('home.html')
-
+@app.route('/', methods=['GET'])
+def signin():
+    hwm = hello_world_main()
+    hello_message = hwm.hello()
+    return render_template('home.html', message=hello_message )
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5002)
